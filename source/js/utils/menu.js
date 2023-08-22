762ac4030198ee68780c06adf2c4ed6071503dd4
Bwomsamdi <113728931+Bwomsamdi@users.noreply.github.com>
2023-08-22 12:29:08 +0300

    Добавляет правки после код-ревью

--- source/js/utils/menu.js
+++ source/js/utils/menu.js
@@ -3,40 +3,42 @@ let navToggle = document.querySelector('.navigation__toggle');
 let bodyElements = document.querySelectorAll('.data-close-menu');
 
 const showMenu = () => {
-  navMain.classList.remove('navigation--nojs');
+  if (navToggle) {
+    navMain.classList.remove('navigation--nojs');
 
-  navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('navigation--closed')) {
-      navMain.classList.remove('navigation--closed');
-      navMain.classList.add('navigation--opened');
-      window.scrollLock.disableScrolling();
-    } else {
-      navMain.classList.add('navigation--closed');
-      navMain.classList.remove('navigation--opened');
-      window.scrollLock.enableScrolling();
-    }
-  });
-
-  document.addEventListener('click', (e) => {
-    const withinBoundaries = e.composedPath().includes(navMain);
-    if (!withinBoundaries) {
-      if (navMain.classList.contains('navigation--opened')) {
-        navMain.classList.add('navigation--closed');
-        navMain.classList.remove('navigation--opened');
-        window.scrollLock.enableScrolling();
-      }
-    }
-  });
-
-  bodyElements.forEach((bodyElement) => {
-    bodyElement.addEventListener('click', function () {
-      if (navMain.classList.contains('navigation--opened')) {
+    navToggle.addEventListener('click', function () {
+      if (navMain.classList.contains('navigation--closed')) {
+        navMain.classList.remove('navigation--closed');
+        navMain.classList.add('navigation--opened');
+        window.scrollLock.disableScrolling();
+      } else {
         navMain.classList.add('navigation--closed');
         navMain.classList.remove('navigation--opened');
         window.scrollLock.enableScrolling();
       }
     });
-  });
+
+    document.addEventListener('click', (e) => {
+      const withinBoundaries = e.composedPath().includes(navMain);
+      if (!withinBoundaries) {
+        if (navMain.classList.contains('navigation--opened')) {
+          navMain.classList.add('navigation--closed');
+          navMain.classList.remove('navigation--opened');
+          window.scrollLock.enableScrolling();
+        }
+      }
+    });
+
+    bodyElements.forEach((bodyElement) => {
+      bodyElement.addEventListener('click', function () {
+        if (navMain.classList.contains('navigation--opened')) {
+          navMain.classList.add('navigation--closed');
+          navMain.classList.remove('navigation--opened');
+          window.scrollLock.enableScrolling();
+        }
+      });
+    });
+  }
 };
 
 export {showMenu};
